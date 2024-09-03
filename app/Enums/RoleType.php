6512d1d4d633e9a93a8ec66510afb0508e55115e
tvkrunal <krunal@techvoot.com>
2024-09-03 18:54:45 +0530

    add enum

--- app/Enums/RoleType.php
+++ app/Enums/RoleType.php
@@ -6,11 +6,9 @@ use BenSampo\Enum\Enum;
 
 final class RoleType extends Enum
 {
-    const ADMINISTRATOR = 'Administrator';
     const ADMIN = 'Admin';
-    const HR = 'HR';
-    const PROJECT_MANAGER = 'Project Manager';
-    const TEAM_LEADER = 'Team Leader';
-    const EMPLOYEE = 'Employee';
-    const MARKETING = 'Marketing';
+    const DESIGNER = 'Designer';
+    const PRINTING = 'Printing';
+    const PROCESSOR = 'Processor';
+    const ACCOUNTANT = 'Accountant';
 }
