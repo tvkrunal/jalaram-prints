9f2d414f2b624279de9346f70b3f910b15667fcf
tvkrunal <krunal@techvoot.com>
2024-09-10 17:53:00 +0530

    some condition changes

--- app/Http/Controllers/Admin/InquiryController.php
+++ app/Http/Controllers/Admin/InquiryController.php
@@ -126,16 +126,14 @@ class InquiryController extends Controller implements HasMiddleware
                     $actions .= '<a href="javascript:;" data-url="' . route('inquiry.destroy',$data->id) . '" class="btn btn-sm btn-square btn-neutral text-danger-hover modal-popup-delete" Title="Delete"><i class="fa fa-trash-o"></i></a>';
                 }
 
-                if (Gate::allows('Inquiry Update Stage') && !isset($data->billing) ) {
-                    $actions .= '<a href="javascript:;" data-url="'. route('update.inquiry.stage') . '" data-id="'.$data->id.'" data-stage="'.$data->status.'"class="btn btn-sm btn-square btn-neutral text-danger-hover update-stage" Title="Update Stage"><i class="fa fa-arrow-right"></i></a>';
+                if (Gate::allows('Inquiry Update Stage') && !empty($data->billing) ) {
+                    if($data->status != 5) {
+                        $actions .= '<a href="javascript:;" data-url="'. route('update.inquiry.stage') . '" data-id="'.$data->id.'" data-stage="'.$data->status.'"class="btn btn-sm btn-square btn-neutral text-danger-hover update-stage" Title="Update Stage"><i class="fa fa-arrow-right"></i></a>';
+                    }
                 }
 
                 if (Gate::allows('Inquiry Billing') && Auth::user()->hasRole('Admin') || Auth::user()->hasRole('Accountant')) {
-                    if(isset($data->billing) && ($data->status == 4)) {
-                        $actions .= '<a href="javascript:;" data-url="'. route('update.inquiry.stage') . '" data-id="'.$data->id.'" data-stage="'.$data->status.'"class="btn btn-sm btn-square btn-neutral text-danger-hover update-stage" Title="Update Stage"><i class="fa fa-arrow-right"></i></a>';
-                    }
-
-                    if($data->status == 4 && !($data->billing)) {
+                    if(empty($data->billing)) {
                         $actions .= '<a href="'. route('inquiry.billing',$data->id).'" class="btn btn-sm btn-square btn-neutral text-danger-hover inquiry-billing" Title="Billing"><i class="fa fa-money"></i></a>';
                     }
                 }
